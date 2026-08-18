4b1c10a7e58f7130e0f32b36f005b78993146d93
kialburg <71294205+kialburg@users.noreply.github.com>
2026-08-17 20:59:12 -0500

    output eda

--- KPIs/Assignment Coverage.sql
+++ KPIs/Assignment Coverage.sql
@@ -2,8 +2,8 @@
 -- Report the percentage of successfully assigned digital sessions and any rejected or unassigned sessions.
 
 SELECT 
-COUNT(1),
-FROM digital_20250904_sessions dig
-LEFT JOIN output_sessions_20250904 out
+COUNT(1)
+FROM digital_20250907_sessions dig
+LEFT JOIN output_sessions_20250907 out
 ON dig.session_id = out.session_id
 where out.session_id IS NULL;
